6c311de58a86cd77702090d6e44b453f61dcf1c8
Gavin Lawrence Picard <gpicard@hydra1.eecs.utk.edu>
2026-04-06 18:00:49 -0400

    first commit

---
+++ src/b_tree.c
@@ -0,0 +1,25 @@
+#include "b_tree.h"
+#include "stdlib.h"
+
+void *b_tree_create(char *filename, long size, int key_size) {
+  return NULL;
+}
+void *b_tree_attach(char *filename) {
+  return NULL;
+}
+
+unsigned int b_tree_insert(void *b_tree, void *key, void *record) {
+  return 0;
+}
+unsigned int b_tree_find(void *b_tree, void *key) {
+  return 0;
+}
+void *b_tree_disk(void *b_tree) {
+  return NULL;
+}
+int b_tree_key_size(void *b_tree) {
+  return 0;
+}
+void b_tree_print_tree(void *b_tree) {
+  return;
+}
